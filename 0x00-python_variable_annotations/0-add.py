955c7f234f5306ce5d32206ac09f135e4a508def
Rahmat <rahmatbola3@gmail.com>
2024-07-05 02:30:58 +0000

    advancedPython

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module for Basic annotations - add
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two float numbers and returns the sum as a float
+    """
+    return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''
+Module for  Basic annotations - concatenation.
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''Concatenates two strings and returns the result.
+    '''
+    return str1 + str2
